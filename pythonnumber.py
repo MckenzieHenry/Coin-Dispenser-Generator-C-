a85057959fa05b414f5d7766bc5c11fabfececcc
Mckenzie Henry <128160364+MckenzieHenry@users.noreply.github.com>
2023-03-21 16:26:44 -0500

    Add files via upload

---
+++ pythonnumber.py
@@ -0,0 +1,30 @@
+#1)
+
+x = input("Please enter a number for x: ")
+y = input("please enter a number for y: ")
+
+if x < y:
+      print("x is smaller than y")
+
+if x == y:
+      print("x is equal to y")
+
+elif x > y:
+      print("y is smaller than x")
+#2)
+x = int(input())
+
+if x == 100:
+      print("Value of the variable is 100")
+
+elif x == 200:
+      print("Value of the variable is 200")
+
+elif x == 300:
+      print("Value of the variable is 300")
+
+elif x == 400:
+      print("Value of the variable is 400")
+
+else:
+       print ("Not found")
